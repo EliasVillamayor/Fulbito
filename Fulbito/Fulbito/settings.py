4c1b1379b8c05cf15af4a32f10f9a54546886650
Elias <eliasvillamayorprofesional@gmail.com>
2024-08-27 14:58:39 -0300

    removido user

--- Fulbito/Fulbito/settings.py
+++ Fulbito/Fulbito/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'fulbitoApi'
+    'fulbitoApi',
+    'corseheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173/',
 ]
 
 ROOT_URLCONF = 'Fulbito.urls'
--- Fulbito/fulbitoApi/views.py
+++ Fulbito/fulbitoApi/views.py
@@ -6,48 +6,6 @@ from .models import *
 from .serializers import *
 # Create your views here.
 
-#
-#
-#METODOS DE USUARIO
-#
-#
-@api_view(['GET'])
-def get_all_users(request):
-    users = User.objects.all()
-    users_serialized = UserSerializer(users, many=True)
-    return Response(users_serialized.data)
-
-@api_view(['POST'])
-def create_user(request):
-    serializer = UserSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def user_detail(request, pk):
-    try:
-        user = User.objects.get(pk=pk)
-    except User.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    if request.method == 'GET':
-        user_serialized = UserSerializer(user)
-        return Response(user_serialized.data)
-    
-
-    elif request.method == 'PUT':
-        user_serialized = UserSerializer(user, data=request.data)
-        if user_serialized.is_valid():
-            user_serialized.save()
-            return Response(user_serialized.data)
-        return Response(user_serialized.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    if request.method == 'DELETE':
-        user.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    
 
 #
 #
